88c5f7a166835d5edce933a98123d291f232fb17
nomanrazasial <nomanrazasial@gmail.com>
2026-09-19 00:09:32 +0200

    Update covers.js

--- dist/assets/journal-covers/covers.js
+++ dist/assets/journal-covers/covers.js
@@ -1,50 +1,57 @@
-window.JOURNAL_COVERS={
-  "energy":{
-    image:"assets/journal-covers/energy-cover.png",
-    publisher:"Elsevier",
-    caption:"Energy",
-    url:"https://www.sciencedirect.com/journal/energy"
-  }
-  "international journal of hydrogen energy":{
-    image:"assets/journal-covers/ijhe-cover.jpg",
-    publisher:"Elsevier",
-    caption:"International Journal of Hydrogen Energy",
-    url:"https://www.sciencedirect.com/journal/international-journal-of-hydrogen-energy"
-  }
-  "international journal of thermofluids":{
-    image:"assets/journal-covers/ijotf-cover.jpg",
-    publisher:"Elsevier",
-    caption:"International Journal of Thermofluids",
-    url:"https://www.sciencedirect.com/journal/international-journal-of-thermofluids"
-  }
-  "energy conversion and management":{
-    image:"assets/journal-covers/ecm-cover.jpg",
-    publisher:"Elsevier",
-    caption:"Energy Conversion and Management",
-    url:"https://www.sciencedirect.com/journal/energy-conversion-and-management"
-  }
-  "sustainable cities and society":{
-    image:"assets/journal-covers/scas-cover.jpg",
-    publisher:"Elsevier",
-    caption:"Sustainable Cities and Society",
-    url:"https://www.sciencedirect.com/journal/sustainable-cities-and-society"
-  }
-  "chemosphere":{
-    image:"assets/journal-covers/chemosphere-cover.jpg",
-    publisher:"Elsevier",
-    caption:"Chemosphere",
-    url:"https://www.sciencedirect.com/journal/chemosphere"
-  }
-  "acs sustainable chemistry and engineering":{
-    image:"assets/journal-covers/ascecg-cover.jpg",
-    publisher:"Elsevier",
-    caption:"ACS Sustainabel Chemistry and Engineering",
-    url:"https://pubs.acs.org/ascecg"
-  }
-  "energy proceedings volume 24 (2021)":{
-    image:"assets/journal-covers/ep-cover.png",
-    publisher:"Elsevier",
-    caption:"Energy Proceedings 24 (2021)",
-    url:"https://www.energy-proceedings.org"
+window.JOURNAL_COVERS = {
+  "energy": {
+    image: "assets/journal-covers/energy-cover.png",
+    publisher: "Elsevier",
+    caption: "Energy",
+    url: "https://www.sciencedirect.com/journal/energy"
+  },
+
+  "international journal of hydrogen energy": {
+    image: "assets/journal-covers/ijhe-cover.jpg",
+    publisher: "Elsevier",
+    caption: "International Journal of Hydrogen Energy",
+    url: "https://www.sciencedirect.com/journal/international-journal-of-hydrogen-energy"
+  },
+
+  "international journal of thermofluids": {
+    image: "assets/journal-covers/ijotf-cover.jpg",
+    publisher: "Elsevier",
+    caption: "International Journal of Thermofluids",
+    url: "https://www.sciencedirect.com/journal/international-journal-of-thermofluids"
+  },
+
+  "energy conversion and management": {
+    image: "assets/journal-covers/ecm-cover.jpg",
+    publisher: "Elsevier",
+    caption: "Energy Conversion and Management",
+    url: "https://www.sciencedirect.com/journal/energy-conversion-and-management"
+  },
+
+  "sustainable cities and society": {
+    image: "assets/journal-covers/scas-cover.jpg",
+    publisher: "Elsevier",
+    caption: "Sustainable Cities and Society",
+    url: "https://www.sciencedirect.com/journal/sustainable-cities-and-society"
+  },
+
+  "chemosphere": {
+    image: "assets/journal-covers/chemosphere-cover.jpg",
+    publisher: "Elsevier",
+    caption: "Chemosphere",
+    url: "https://www.sciencedirect.com/journal/chemosphere"
+  },
+
+  "acs sustainable chemistry and engineering": {
+    image: "assets/journal-covers/ascecg-cover.jpeg",
+    publisher: "ACS",
+    caption: "ACS Sustainable Chemistry & Engineering",
+    url: "https://pubs.acs.org/journal/ascecg"
+  },
+
+  "energy proceedings volume 24 2021": {
+    image: "assets/journal-covers/ep-cover.png",
+    publisher: "Energy Proceedings",
+    caption: "Energy Proceedings, Volume 24 (2021)",
+    url: "https://www.energy-proceedings.org"
   }
 };
